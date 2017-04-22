95ab8b2227a7a9d809b92dd7f266a78666d8308a
brandelli <bdbrandelli@hotmail.com>
2017-04-22 15:36:12 -0300

    teste de funcionamento do sequencial com alocacao dinamica

--- trabalho1/testes.c
+++ trabalho1/testes.c
@@ -5,11 +5,12 @@
 #define ROWS 10
 #define COLUMNS 10
 
-int vet[ROWS][COLUMNS];
 int compare(const void* a, const void* b);
 
 void main (int argc, const char* argv[]){
+  int **vet = malloc(ROWS * sizeof (int *));
   for (int i = 0; i < ROWS; i++){
+    vet[i] = malloc(COLUMNS * sizeof(int));
     for (int j = 0; j < COLUMNS; j++){
       vet[i][j] = rand()%10;
     }
@@ -30,6 +31,7 @@ void main (int argc, const char* argv[]){
     }
     printf("\n");
   }
+  free(vet);
 }
 
 int compare (const void* a, const void* b){
